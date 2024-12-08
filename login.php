ed90f0cd541f518b5e25641844366c528ab97e83
Khoa Nguyen <166854304+Jaime1108@users.noreply.github.com>
2024-12-08 02:12:37 -0600

    Update login.php

--- login.php
+++ login.php
@@ -55,7 +55,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - RowdyBookly</title>
-    <link rel="stylesheet" href="css/style.css">
     <style>
         /* Centering the login container */
         .login-container {
